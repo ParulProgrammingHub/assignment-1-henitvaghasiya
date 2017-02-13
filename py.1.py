ebebd44e9ee27e797ef251755fcd8de6fce25497
henitvaghasiya <henitvaghasiya12345@gmail.com>
2017-02-13 13:56:32 +0530

    Add files via upload

---
+++ py.1.py
@@ -0,0 +1,5 @@
+l=input("enter the length of rectangle:")
+b=input("enter breadth of rectangle:")
+area =l*b
+perimeter=2*(l+b)
+print "area and perimeter of rectangel are %d and %d respectiveliy" % (area,perimeter)
